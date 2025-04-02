b1de55f55ba3b3a73c74c30c1dbb77e3f7e88e03
Lnta-Li <1107127328@qq.com>
2025-04-02 23:26:20 +0800

    update

--- templets/default/js/mobile-menu.js
+++ templets/default/js/mobile-menu.js
@@ -8,7 +8,6 @@ document.addEventListener('DOMContentLoaded', function() {
         // 点击菜单按钮时切换导航菜单的显示状态
         menuToggle.addEventListener('click', function() {
             mobileNav.classList.toggle('active');
-            var pageBody = document.getElementById('pagebody');
             
             // 切换菜单按钮的样式
             var spans = menuToggle.querySelectorAll('span');
@@ -16,12 +15,10 @@ document.addEventListener('DOMContentLoaded', function() {
                 spans[0].style.transform = 'rotate(45deg) translate(5px, 5px)';
                 spans[1].style.opacity = '0';
                 spans[2].style.transform = 'rotate(-45deg) translate(7px, -7px)';
-                pageBody.classList.add('navopen');
             } else {
                 spans[0].style.transform = 'none';
                 spans[1].style.opacity = '1';
                 spans[2].style.transform = 'none';
-                pageBody.classList.remove('navopen');
             }
         });
         
@@ -29,14 +26,12 @@ document.addEventListener('DOMContentLoaded', function() {
         document.addEventListener('click', function(event) {
             if (!mobileNav.contains(event.target) && !menuToggle.contains(event.target) && mobileNav.classList.contains('active')) {
                 mobileNav.classList.remove('active');
-                document.getElementById('indexbody').classList.remove('navmenuopen');
                 
                 // 恢复菜单按钮样式
                 var spans = menuToggle.querySelectorAll('span');
                 spans[0].style.transform = 'none';
                 spans[1].style.opacity = '1';
                 spans[2].style.transform = 'none';
-                document.getElementById('pagebody').classList.remove('navopen');
             }
         });
     }
