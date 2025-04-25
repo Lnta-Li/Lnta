540bb92369230ea446eb3d38813d3d5426847a61
Lnta-Li <1107127328@qq.com>
2025-04-25 23:27:43 +0800

    update

--- templets/default/js/image-carousel.js
+++ templets/default/js/image-carousel.js
@@ -1,5 +1,4 @@
 /** 图片轮播组件 */
-
 class ImageCarousel {
     constructor(container) {
         this.container = container;
@@ -267,14 +266,6 @@ class ImageCarousel {
 
 // DOM加载完成后初始化轮播
 document.addEventListener('DOMContentLoaded', () => {
-    // 添加CSS样式链接
-    const cssLink = document.createElement('link');
-    cssLink.href = '/templets/default/style/image-carousel.css';
-    cssLink.rel = 'stylesheet';
-    cssLink.media = 'screen';
-    cssLink.type = 'text/css';
-    document.head.appendChild(cssLink);
-    
     const carouselContainers = document.querySelectorAll('.image-carousel');
     carouselContainers.forEach(container => {
         new ImageCarousel(container);
